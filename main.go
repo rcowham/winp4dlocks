6d4b4e85c1ac1875535a2567d97163324d295b15
Robert Cowham <rcowham@perforce.com>
2026-01-23 17:01:27 +0000

    Version of find process which works

--- main.go
+++ main.go
@@ -3,6 +3,10 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"strings"
+	"syscall"
 	"unsafe"
 
 	"golang.org/x/sys/windows"
@@ -24,30 +28,12 @@ type WAITCHAIN_NODE_INFO struct {
 }
 
 var (
-	kernel32                        = windows.NewLazyDLL("kernel32.dll")
+	// kernel32                        = windows.NewLazyDLL("kernel32.dll")
 	advapi32                        = windows.NewLazyDLL("advapi32.dll")
 	procOpenThreadWaitChainSession  = advapi32.NewProc("OpenThreadWaitChainSession")
 	procGetThreadWaitChain          = advapi32.NewProc("GetThreadWaitChain")
 	procCloseThreadWaitChainSession = advapi32.NewProc("CloseThreadWaitChainSession")
 )
-
-func findProcessByName(name string) uint32 {
-	snap, _ := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
-	defer windows.CloseHandle(snap)
-
-	var pe windows.ProcessEntry32
-	pe.Size = uint32(unsafe.Sizeof(pe))
-
-	for windows.Process32First(snap, &pe) == nil {
-		if windows.UTF16ToString(pe.ExeFile[:]) == name {
-			return pe.ProcessID
-		}
-		if windows.Process32Next(snap, &pe) != nil {
-			break
-		}
-	}
-	return 0
-}
 
 func getFileNameFromHandle(h windows.Handle) string {
 	buf := make([]uint16, 1024)
@@ -109,12 +95,85 @@ func checkThreadBlockers(tid uint32, session uintptr) {
 	}
 }
 
-func main() {
-	p4pid := findProcessByName("p4s.exe")
-	if p4pid == 0 {
-		panic("p4s.exe not running")
+// WindowsProcess is an implementation of Process for Windows.
+type WindowsProcess struct {
+	ProcessID       int
+	ParentProcessID int
+	Exe             string
+}
+
+func processes() ([]WindowsProcess, error) {
+	handle, err := windows.CreateToolhelp32Snapshot(windows.TH32CS_SNAPPROCESS, 0)
+	if err != nil {
+		return nil, err
+	}
+	defer windows.CloseHandle(handle)
+
+	var entry windows.ProcessEntry32
+	entry.Size = uint32(unsafe.Sizeof(entry))
+	// get the first process
+	err = windows.Process32First(handle, &entry)
+	if err != nil {
+		return nil, err
 	}
 
+	results := make([]WindowsProcess, 0, 50)
+	for {
+		results = append(results, newWindowsProcess(&entry))
+
+		err = windows.Process32Next(handle, &entry)
+		if err != nil {
+			// windows sends ERROR_NO_MORE_FILES on last process
+			if err == syscall.ERROR_NO_MORE_FILES {
+				return results, nil
+			}
+			return nil, err
+		}
+	}
+}
+
+func findProcessByName(processes []WindowsProcess, name string) *WindowsProcess {
+	for _, p := range processes {
+		if strings.ToLower(p.Exe) == strings.ToLower(name) {
+			return &p
+		}
+	}
+	return nil
+}
+
+func newWindowsProcess(e *windows.ProcessEntry32) WindowsProcess {
+	// Find when the string ends for decoding
+	end := 0
+	for {
+		if e.ExeFile[end] == 0 {
+			break
+		}
+		end++
+	}
+
+	return WindowsProcess{
+		ProcessID:       int(e.ProcessID),
+		ParentProcessID: int(e.ParentProcessID),
+		Exe:             syscall.UTF16ToString(e.ExeFile[:end]),
+	}
+}
+
+func main() {
+
+	procs, err := processes()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, p := range procs {
+		fmt.Println(p.Exe)
+	}
+	p4p := findProcessByName(procs, "p4s.exe")
+	if p4p == nil {
+		log.Fatal("p4s.exe not running")
+	}
+	// found it
+	fmt.Printf("p4s.exe pid: %d\n", p4p.ProcessID)
+	p4pid := uint32(p4p.ProcessID)
 	handles := enumP4DBHandles(p4pid)
 	println("db.* handles:", len(handles))
 
